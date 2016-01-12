11bffec19b6e79ebe94852292ad2adc18f8d5076
Basavanagowda Kanur <gowda@unbxd.com>
2016-01-13 03:49:03 +0530

    app - use 'commander' module to parse command-line arguments

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var hello = require('./lib/hello');
+var program = require('commander');
 
 (function() {
-  hello.ask_name(function(name) {
-    hello.say_hello(name);
-  });
+  program.parse(process.argv);
+
+  if (program.args.length == 0) {
+    hello.ask_name(function(name) {
+      hello.say_hello(name);
+    });
+  } else {
+    hello.say_hello(program.args[0]);
+  }
 })();
